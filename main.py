c14ff9f40ceef5e5fad9a3742d7e62bee2029ffc
Kyle Reto <kylereto@gmail.com>
2024-12-05 20:17:43 -0500

    Reworked to remove archived inputs feature

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 import argparse
 import importlib
 import re
-from os import listdir, path
+import sys
+from traceback import format_exc
+from os import listdir
 from utils import abstract_day
 
 parser = argparse.ArgumentParser(description='Run the AoC problem solutions.')
@@ -20,34 +22,72 @@ for filename in listdir("days"):
 
 parser.add_argument("-d", "--day", help="The day of code to run", type=int,
                     choices=[i for i in range(1,max_day+1)], default=max_day)
-parser.add_argument("-s", "--sample-only",
+parser.add_argument("-v", "--verbose",
+                    help='print developer-facing exceptions to the command line (instead of '
+                    'user-facing warnings)', action="store_true")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-s", "--sample_only",
                     help='run only the sample (excluding the full input)', action="store_true")
+group.add_argument("-i", "--input-only",
+                    help='run only the full input (excluding the sample)', action="store_true")
 args = parser.parse_args()
 
 # Find the associated module by name and run it.
 day_module = importlib.import_module("days.day_" + str(args.day))
 day_code: abstract_day = day_module.DayCode
-try:
-    with open("sample.txt", "r", encoding="utf-8") as f:
-        test_str = f.read()
-    print("Part 1 Sample Output: ", day_code.part_1(test_str))
-    print("Part 2 Sample Output: ", day_code.part_2(test_str))
-except FileNotFoundError:
-    print("No sample.txt file was found, skipping sample runs...\n"
-          "If you'd like to run the smaller input samples provided by AoC separately from the main "
-          "problem input, create a file called `sample.txt` and place it in the AOC2024 "
-          "directory, then paste the sample input into that file.\n")
+input_url = f"https://adventofcode.com/2024/day/{args.day}/input"
+
+if not args.input_only:
+    try:
+        with open("sample.txt", "r", encoding="utf-8") as f:
+            test_str = f.read()
+        try:
+            print(f"Day {args.day} Part 1 Sample Output: {day_code.part_1(test_str)}")
+            print(f"Day {args.day} Part 2 Sample Output: {day_code.part_2(test_str)}")
+        # Exceptions here will always be due to user error, and the type is unpredictable
+        except Exception as e: # pylint: disable=broad-exception-caught
+            if not args.verbose:
+                sys.exit("An unexpected exception occurred when attempting to run "
+                            f"the solution for day {args.day}. Make sure your input.txt file "
+                        f"is the one for day {args.day}, which you can find here: {input_url}."
+                        "\nIf you meant to run a different day, use the --day flag."
+                        "\nIf you want to print the exception instead of this message, use "
+                        "the --verbose flag\n")
+            else:
+                sys.exit(format_exc())
+    except FileNotFoundError:
+        WARNING = ("No sample.txt file was found, skipping sample runs...\n"
+            "If you'd like to run the smaller input samples provided by AoC separately from the "
+            "main problem input, create a file called `sample.txt` and place it in the AOC2024 "
+            "directory, then paste the sample input into that file.\n"
+            "If you don't want to run the smaller input samples, use the --input-only flag "
+            "to silence this warning.\n")
+        if args.sample_only:
+            sys.exit(WARNING)
+        print(WARNING)
+
 
 if not args.sample_only:
     try:
         with open("input.txt", "r", encoding="utf-8") as f:
             in_str = f.read()
     except FileNotFoundError:
-        print("No input.txt file was found, defaulting to archived inputs...\n"
-            "If you'd like to run this code on a custom input, please create a file "
+        sys.exit("No input.txt file was found, so the solution could not be run.\n"
+            "If you'd like to run this solution, please create a file "
             "with your desired input text called `input.txt` and place it in the "
             "AOC2024 directory.\n")
-        with open(path.join("inputs",f"day_{args.day}.txt"), "r", encoding="utf-8") as f:
-            in_str = f.read()
-    print("Part 1 Full Output:   ", day_code.part_1(in_str))
-    print("Part 2 Full Output:   ", day_code.part_2(in_str))
+    try:
+        print(f"Day {args.day} Part 1 Full Output: {day_code.part_1(in_str)}")
+        print(f"Day {args.day} Part 2 Full Output: {day_code.part_2(in_str)}")
+
+    # Exceptions here will always be due to user error, and the type is unpredictable
+    except Exception as e: # pylint: disable=broad-exception-caught
+        if not args.verbose:
+            sys.exit("An unexpected exception occurred when attempting to run "
+                    f"the solution for day {args.day}. Make sure your input.txt file "
+                f"is the one for day {args.day}, which you can find here: {input_url}."
+                "\nIf you meant to run a different day, use the --day flag."
+                "\nIf you want to print the exception instead of this message, use "
+                "the --verbose flag\n")
+        else:
+            sys.exit(format_exc())
